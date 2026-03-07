a6914927f73204c8528a7f81002b98e4da2dbec6
delphi <124740881+De1p0@users.noreply.github.com>
2026-03-06 23:13:33 -0500

    a

--- sources/allanime.js
+++ sources/allanime.js
@@ -1,4 +1,5 @@
-// SOURCE CODE ADAPTED FROM https://github.com/kodjodevf/mangayomi-extensions 
+// SOURCE CODE ADAPTED FROM https://github.com/kodjodevf/mangayomi-extensions
+
 const _storage = {};
 
 function sendMessage(action, payload) {
@@ -20,19 +21,23 @@ function sendMessage(action, payload) {
             return null;
     }
 }
+
 class SharedPreferences {
     get(key) {
         return sendMessage("get", JSON.stringify([key]));
     }
+
     getString(key, defaultValue) {
         return sendMessage("getString", JSON.stringify([key, defaultValue]));
     }
-    setString(key, defaultValue) {
-        return sendMessage("setString", JSON.stringify([key, defaultValue]));
+
+    setString(key, value) {
+        return sendMessage("setString", JSON.stringify([key, value]));
     }
 }
 
 export class DefaultExtension {
+
     constructor(corFetch, source = {
         name: "AllManga",
         langs: ["en"],
@@ -40,22 +45,27 @@ export class DefaultExtension {
         apiUrl: "https://api.allanime.day/api",
         typeSource: "single",
         itemType: 0,
-        version: "0.1.0",
+        version: "0.1.1",
     }) {
         this.source = source;
         this.corFetch = corFetch;
     }
 
-    async fetchUrl(url, headers = {}) {
-        const res = await this.corFetch(url, { headers });
-        if (!res.ok) throw new Error(`HTTP error ${res.status}`);
+    async fetchUrl(url, options = {}) {
+
+        const res = await this.corFetch(url, options);
+
+        if (!res.ok) {
+            throw new Error(`HTTP error ${res.status}`);
+        }
+
         return { body: await res.text() };
     }
 
     getHeaders() {
         return {
             "Accept": "*/*",
-            "referer": this.source.baseUrl,
+            "referer": "https://allmanga.to/",
             "user-agent": this.preferenceUserAgent(),
         };
     }
@@ -67,13 +77,24 @@ export class DefaultExtension {
         };
     }
 
-    async getPopular(page) {
-        const body = JSON.stringify({
-            query: Queries.popularMangaQuery,
-            variables: { page, size: 20, type: "manga", dateRange: 0, allowAdult: false, allowUnknown: false }
+    async post(body) {
+
+        return this.fetchUrl(this.source.apiUrl, {
+            method: "POST",
+            headers: this.getPostHeaders(),
+            body: JSON.stringify(body)
         });
-        const res = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+
+    }
+
+    async getPopular(page) {
+
+        const res = await this.post(
+            Queries.buildPopularMangaQuery(page)
+        );
+
         const data = JSON.parse(res.body)?.data?.queryPopular?.recommendations ?? [];
+
         return data.map(item => ({
             name: MangaUtils.getMangaName(item.anyCard),
             imageUrl: URLS.buildMangaCoverUrl(item.anyCard.thumbnail),
@@ -82,9 +103,13 @@ export class DefaultExtension {
     }
 
     async getLatestUpdates(page) {
-        const body = JSON.stringify(Queries.buildSearchQuery({ page }));
-        const res = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+
+        const res = await this.post(
+            Queries.buildSearchQuery({ page })
+        );
+
         const items = JSON.parse(res.body)?.data?.mangas?.edges ?? [];
+
         return items.map(item => ({
             name: MangaUtils.getMangaName(item),
             imageUrl: URLS.buildMangaCoverUrl(item.thumbnail),
@@ -92,10 +117,14 @@ export class DefaultExtension {
         }));
     }
 
-    async search(query, page, filters) {
-        const body = JSON.stringify(Queries.buildSearchQuery({ page, query }));
-        const res = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+    async search(query, page) {
+
+        const res = await this.post(
+            Queries.buildSearchQuery({ page, query })
+        );
+
         const items = JSON.parse(res.body)?.data?.mangas?.edges ?? [];
+
         return items.map(item => ({
             name: MangaUtils.getMangaName(item),
             imageUrl: URLS.buildMangaCoverUrl(item.thumbnail),
@@ -104,21 +133,36 @@ export class DefaultExtension {
     }
 
     async getDetail(url) {
+
         const mangaId = url.split("/").pop();
-        const resDetail = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+
+        const resDetail = await this.post(
+            Queries.buildDetailsQuery(mangaId)
+        );
+
         const detailsData = JSON.parse(resDetail.body)?.data?.manga;
-        const resChapters = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+
+        const resChapters = await this.post(
+            Queries.buildChaptersQuery(mangaId)
+        );
+
         const chaptersData = JSON.parse(resChapters.body)?.data?.episodeInfos ?? [];
 
-        const chapters = chaptersData.sort((a, b) => b.episodeIdNum - a.episodeIdNum)
+        const chapters = chaptersData
+            .sort((a, b) => b.episodeIdNum - a.episodeIdNum)
             .map(cur => {
+
                 const episodeNumber = cur.episodeIdNum ?? "Unknown";
+
                 return {
                     name: `Chapter ${episodeNumber}`,
-                    dateUpload: cur.uploadDates?.sub ? new Date(cur.uploadDates.sub).valueOf() : null,
+                    dateUpload: cur.uploadDates?.sub
+                        ? new Date(cur.uploadDates.sub).valueOf()
+                        : null,
                     description: cur.notes,
-                    url: URLS.buildMangaURL(`${mangaId}/chapter-${episodeNumber}-sub`),
+                    url: URLS.buildMangaURL(`${mangaId}/chapter-${episodeNumber}-sub`)
                 };
+
             });
 
         return {
@@ -135,24 +179,45 @@ export class DefaultExtension {
     }
 
     async getPageList(url) {
+
         const parts = url.split("/");
-        if (parts.length < 3) return [];
+
         const mangaId = parts[parts.length - 2];
         const chapterNum = parts[parts.length - 1].split("-")[1];
         const chapterType = parts[parts.length - 1].split("-").pop();
-        const body = JSON.stringify(Queries.buildPageQuery({ id: mangaId, chapterNum, translationType: chapterType }));
-        const res = await this.fetchUrl(this.source.apiUrl, this.getPostHeaders());
+
+        const res = await this.post(
+            Queries.buildPageQuery({
+                id: mangaId,
+                chapterNum,
+                translationType: chapterType
+            })
+        );
+
         const pagesData = JSON.parse(res.body)?.data?.chapterPages?.edges?.[0];
+
         const pictureUrlHead = pagesData.pictureUrlHead;
+
         return pagesData.pictureUrls.map(page => ({
-            url: URLS.addHttp(URLS.buildMangaPageUrl(pictureUrlHead, page.url, this.preferenceImageQuality())),
+            url: URLS.addHttp(
+                URLS.buildMangaPageUrl(
+                    pictureUrlHead,
+                    page.url,
+                    this.preferenceImageQuality()
+                )
+            ),
             headers: this.getHeaders()
         }));
     }
 
     preferenceUserAgent() {
-        const userAgent = new SharedPreferences().getString("USERAGENT", "").trim();
-        return userAgent || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/143.0.0.0 Safari/537.36";
+
+        const userAgent = new SharedPreferences()
+            .getString("USERAGENT", "")
+            .trim();
+
+        return userAgent ||
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36";
     }
 
     preferenceImageQuality() {
@@ -160,42 +225,163 @@ export class DefaultExtension {
     }
 }
 
-// Minimal supporting objects
 const URLS = {
-    buildMangaCoverUrl: url => url.startsWith("http") ? url : `https://wp.youtube-anime.com/aln.youtube-anime.com/${url}`,
-    buildMangaURL: id => `https://allmanga.to/manga/${id}`,
+
+    buildMangaCoverUrl: url =>
+        url.startsWith("http")
+            ? url
+            : `https://wp.youtube-anime.com/aln.youtube-anime.com/${url}`,
+
+    buildMangaURL: id =>
+        `https://allmanga.to/manga/${id}`,
+
     buildMangaPageUrl: (head, path, quality) => {
-        if (quality === "800") return `https://ytimgf.youtube-anime.com/${path}?w=800`;
-        if (quality === "480") return `https://wp.youtube-anime.com/${head}/${path}?w=480`;
+
+        if (quality === "800")
+            return `https://ytimgf.youtube-anime.com/${path}?w=800`;
+
+        if (quality === "480")
+            return `https://wp.youtube-anime.com/${head}/${path}?w=480`;
+
         return `https://ytimgf.youtube-anime.com/${path}`;
     },
-    addHttp: url => url.startsWith("http") ? url : `https://${url}`,
+
+    addHttp: url =>
+        url.startsWith("http") ? url : `https://${url}`,
 };
 
 const MangaUtils = {
-    getMangaName: data => data.englishName || data.name || data.nativeName || "No Title",
-    getAuthor: data => Array.isArray(data.authors) ? data.authors[0] : "None",
+
+    getMangaName: data =>
+        data.englishName ||
+        data.name ||
+        data.nativeName ||
+        "No Title",
+
+    getAuthor: data =>
+        Array.isArray(data.authors)
+            ? data.authors[0]
+            : "None",
+
     getStatus: status => {
+
         if (!status) return 4;
+
         const s = status.toLowerCase();
+
         if (s.includes("complete") || s.includes("finished")) return 1;
+
         if (s.includes("ongoing") || s.includes("releasing") || s.includes("publishing")) return 0;
+
         return 4;
     },
-    buildDescription: (desc, altNames) => {
-        return desc + (altNames.length ? `\n\nAlternative Names:\n${altNames.join("\n")}` : "");
-    },
-    combineGenres: (genres, tags) => [...new Set([...genres, ...tags])],
+
+    buildDescription: (desc, altNames) =>
+        desc +
+        (altNames?.length
+            ? `\n\nAlternative Names:\n${altNames.join("\n")}`
+            : ""),
+
+    combineGenres: (genres, tags) =>
+        [...new Set([...(genres ?? []), ...(tags ?? [])])],
 };
 
 const Queries = {
-    popularMangaQuery: `query ($type: VaildPopularTypeEnumType! $size: Int! $page: Int $dateRange: Int $allowAdult: Boolean $allowUnknown: Boolean) { queryPopular(type: $type size: $size dateRange: $dateRange page: $page allowAdult: $allowAdult allowUnknown: $allowUnknown) { recommendations { anyCard { _id name thumbnail englishName } } } }`,
+
+    popularMangaQuery: `
+query ($type: VaildPopularTypeEnumType! $size: Int! $page: Int $dateRange: Int $allowAdult: Boolean $allowUnknown: Boolean) {
+  queryPopular(type: $type size: $size dateRange: $dateRange page: $page allowAdult: $allowAdult allowUnknown: $allowUnknown) {
+    recommendations {
+      anyCard {
+        _id
+        name
+        thumbnail
+        englishName
+      }
+    }
+  }
+}`,
+
+    buildPopularMangaQuery: page => ({
+        query: Queries.popularMangaQuery,
+        variables: {
+            type: "manga",
+            size: 20,
+            page,
+            dateRange: 0,
+            allowAdult: false,
+            allowUnknown: false
+        }
+    }),
+
     buildSearchQuery: ({ page, query }) => ({
-        query: `query ($search: SearchInput $size: Int $page: Int $translationType: VaildTranslationTypeMangaEnumType $countryOrigin: VaildCountryOriginEnumType) { mangas(search: $search limit: $size page: $page translationType: $translationType countryOrigin: $countryOrigin) { edges { _id name thumbnail englishName } } }`,
-        variables: { page, size: 20, search: { query } }
+        query: `
+query ($search: SearchInput $size: Int $page: Int $translationType: VaildTranslationTypeMangaEnumType $countryOrigin: VaildCountryOriginEnumType) {
+  mangas(search: $search limit: $size page: $page translationType: $translationType countryOrigin: $countryOrigin) {
+    edges {
+      _id
+      name
+      thumbnail
+      englishName
+    }
+  }
+}`,
+        variables: {
+            page,
+            size: 20,
+            search: { query }
+        }
     }),
+
+    buildDetailsQuery: id => ({
+        query: `
+query ($id: String!) {
+  manga(_id: $id) {
+    _id
+    name
+    thumbnail
+    description
+    authors
+    genres
+    tags
+    status
+    altNames
+    englishName
+  }
+}`,
+        variables: { id }
+    }),
+
+    buildChaptersQuery: id => ({
+        query: `
+query ($id: String!, $chapterNumStart: Float!, $chapterNumEnd: Float!) {
+  episodeInfos(showId: $id episodeNumStart: $chapterNumStart episodeNumEnd: $chapterNumEnd) {
+    episodeIdNum
+    notes
+    uploadDates
+  }
+}`,
+        variables: {
+            id: `manga@${id}`,
+            chapterNumStart: 0,
+            chapterNumEnd: 9999
+        }
+    }),
+
     buildPageQuery: ({ id, chapterNum, translationType }) => ({
-        query: `query ($id: String! $translationType: VaildTranslationTypeMangaEnumType! $chapterNum: String!) { chapterPages(mangaId: $id translationType: $translationType chapterString: $chapterNum) { edges { pictureUrls pictureUrlHead } } }`,
-        variables: { id, chapterNum, translationType },
-    }),
+        query: `
+query ($id: String! $translationType: VaildTranslationTypeMangaEnumType! $chapterNum: String!) {
+  chapterPages(mangaId: $id translationType: $translationType chapterString: $chapterNum) {
+    edges {
+      pictureUrls
+      pictureUrlHead
+    }
+  }
+}`,
+        variables: {
+            id,
+            chapterNum,
+            translationType
+        }
+    })
 };
